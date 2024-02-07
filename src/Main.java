5cda64c595be0dda34f6b37e0236e85eee8a0438
saeephalke <saeesphalke@gmail.com>
2024-02-07 08:41:34 -0800

    adding images

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-	}
+		APImage image = new APImage("smokey.jpg");
+		image.draw();
 
+	}
 }
