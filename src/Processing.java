c59c56e057104354baed326009c1b3b3d6820688
FeliciaChiu <132633991+FeliciaChiu@users.noreply.github.com>
2024-02-07 09:43:47 -0800

    todo list

--- src/Processing.java
+++ src/Processing.java
@@ -1,4 +1,22 @@
 
 public class Processing {
-
+/**
+* Can use any image
+* Convert an image to BW (Christina)
+* Convert image to grayscale (Christina)
+* Convert image to luminance grayscale (Christina)
+* Rotate left 90, right 90, 180 (Isha)
+* Convert to sepia (Isha)
+* Darken/brighten image (Felicia)
+* Do color filtering (Christina)
+* Posterize image  (Saee)
+* Convert to photographic negative (Saee)
+* Sharpen (Saee)
+* Blur (Saee)
+* Shrink (Felicia)
+* Enlarge (Felicia)
+* VIDEO EDITING (Together)
+* SLIDESSSSS (Together)
+*/
+  
 }
