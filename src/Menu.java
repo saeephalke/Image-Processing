403fc886bd36de5095740fe9469a7e30c517ab1d
saeephalke <saeesphalke@gmail.com>
2024-02-07 09:35:52 -0800

    menu stub

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,35 @@
-
+import images.*;
+import java.util.Scanner;
 public class Menu {
-
+	private String imageFile;
+	private static String[] availableImages = {"smokey.jpg", "swam.jpg", "seagull.jpg", "redMotorcycle.jpg", 
+	                                           "koala.jpg", "butterfly1.jpg", "arch.jpg"};
+	
+	public Menu() {
+		imageFile = "smokey.jpg";
+	}
+	
+	public Menu(String s) {
+		Scanner sc = new Scanner(System.in);
+		while(!isValid(s)) {
+			System.out.println("Try another File Name");
+			s = sc.nextLine();
+			
+		}
+		imageFile = s;
+		
+	}
+	
+	public APImage giveImage() {
+		return new APImage(imageFile);
+	}
+	
+	private boolean isValid(String s) {
+		for(String file : availableImages) {
+			if(file.equals(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
