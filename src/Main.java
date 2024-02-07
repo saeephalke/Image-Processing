1ba1256cac03e08af1a5d75f0a44db715a9f7504
saeephalke <saeesphalke@gmail.com>
2024-02-07 09:09:36 -0800

    creating classes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		APImage image = new APImage("smokey.jpg");
+		APImage image = new APImage("arch.jpg");
 		image.draw();
 
 	}
